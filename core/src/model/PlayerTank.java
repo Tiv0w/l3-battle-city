c307a0a3541d6f4dd539ab7e3769c4cba85268e7
Tiv0w <t.theomeyer@gmail.com>
2021-03-26 12:30:28 +0100

    :construction: Working on collision: almost complete for player

--- core/src/model/PlayerTank.java
+++ core/src/model/PlayerTank.java
@@ -64,55 +64,69 @@ public class PlayerTank extends GameElement {
         float newX = _x + moveX * _speed;
         float newY = _y + moveY * _speed;
         System.out.println("newX Y " + newX + " " + newY);
+        System.out.println("DELTA: " + delta);
 
-        if (isColliding(newX, newY, terrain)) {
-            if (isXCollisionSmall(newX, delta)) {
-                System.out.println("xCollision Petite");
-                _x = newX;
-            }
-            if (isYCollisionSmall(newY, delta)) {
-                System.out.println("yCollision Petite");
-                _y = newY;
-                System.out.println("NOUVEAU _y " + _y);
-            }
-        } else {
+        if (!isColliding(newX, newY, terrain)) {
             _x = newX;
             _y = newY;
+        } else {
+            System.out.println("COLLISION DETECTEE");
+            _x = roundX(newX);
+            _y = roundY(newY);
+            System.out.println(_x);
+            System.out.println(_y);
         }
-
-        System.out.println(_x);
-        System.out.println(_y);
     }
 
     private boolean isColliding(float newX, float newY, Terrain terrain) {
         int col = (int)Math.floor(newX);
         int row = (int)Math.floor(newY);
+        int col2 = (int)Math.floor(newX + 1.8);
+        int row2 = (int)Math.floor(newY + 1.8);
 
         GameElement e1 = terrain.getElement(col, row);
         GameElement e2 = terrain.getElement(col, row);
+        GameElement e3 = terrain.getElement(col, row);
 
         if (_direction == Direction.UP) {
             e1 = terrain.getElement(col, row);
             e2 = terrain.getElement(col + 1, row);
+            e3 = terrain.getElement(col2, row);
         } else if (_direction == Direction.DOWN) {
             e1 = terrain.getElement(col, row + (int)_size);
             e2 = terrain.getElement(col + 1, row + (int)_size);
+            e3 = terrain.getElement(col2, row + (int)_size);
         } else if (_direction == Direction.RIGHT) {
             e1 = terrain.getElement(col + (int)_size, row);
             e2 = terrain.getElement(col + (int)_size, row + 1);
+            e3 = terrain.getElement(col + (int)_size, row2);
         } else if (_direction == Direction.LEFT) {
             e1 = terrain.getElement(col, row);
             e2 = terrain.getElement(col, row + 1);
+            e3 = terrain.getElement(col, row2);
         }
 
         if (e1 instanceof ConcreteWall || e1 instanceof BrickWall ||
-            e2 instanceof ConcreteWall || e2 instanceof BrickWall) {
+            e2 instanceof ConcreteWall || e2 instanceof BrickWall ||
+            e3 instanceof ConcreteWall || e3 instanceof BrickWall) {
             return true;
         }
         System.out.println("yes");
         return
             (col < 0 || col > terrain.getWidth() - 1 - _size) ||
             (row < 0 || row > terrain.getHeight() - 1 - _size);
+    }
+
+    private float roundX(float x) {
+        if (_direction == Direction.RIGHT) return (float)Math.round(x);
+        if (_direction == Direction.LEFT) return (float)Math.round(x);
+        return x;
+    }
+
+    private float roundY(float y) {
+        if (_direction == Direction.UP) return (float)Math.round(y);
+        if (_direction == Direction.DOWN) return (float)Math.round(y);
+        return y;
     }
 
     private boolean isXCollisionSmall(float x, float delta) {
